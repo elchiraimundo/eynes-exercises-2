d6200000c98fab070af03c145f5898decbdd8bb9
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-26 12:53:47 -0300

    Metodo Init

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,8 @@
 import math
 
-class Circle():
-    pass
+class Circle:
+
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("El radio debe ser mayor que 0")
+        self.radius = radius
