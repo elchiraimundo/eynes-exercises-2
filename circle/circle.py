2924cd5adff9c9b36e503950ef525297278a84a7
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-26 15:21:01 -0300

    Clase Circle

--- circle/circle.py
+++ circle/circle.py
@@ -6,3 +6,25 @@ class Circle:
         if radius <= 0:
             raise ValueError("El radio debe ser mayor que 0")
         self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+    
+    def set_radius(self,radius):
+        if radius <= 0:
+            raise ValueError("El radio debe ser mayor que 0")
+        self.radius = radius
+
+    def get_area(self):
+        return 3.14159 * self.radius**2 
+    
+    def get_perimeter(self):
+        return 3.14159 * self.radius * 2
+    
+    def __mul__(self, n):
+        if n <= 0:
+            raise ValueError("El multiplicador debe ser mayor a 0")
+        return Circle(self.radius * n) 
+
+    def __str__(self):
+        return f"El Radio del Circulo es de {self.radius}"
