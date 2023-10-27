1fd6e92df5cbe0298676eb12fd9143a3189cbf42
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-27 15:10:57 -0300

    Metodo Transpose

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,16 @@
+
 def transpose(matrix):
-    pass
+
+    filas = len(matrix)
+    columnas = len(matrix[0])
+
+    matriz_transpuesta = []
+    for _ in range(columnas):
+        fila = [0] * filas
+        matriz_transpuesta.append(fila)
+
+    for i in range(filas):
+        for j in range(columnas):
+            matriz_transpuesta[j][i] = matrix[i][j]
+
+    return matriz_transpuesta
