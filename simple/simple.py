b4b0a59d5142c8d1dc96216f44eb991ec5667dbf
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-27 15:43:12 -0300

    Diccionarios

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,16 @@
 import random
 
-
+# Creo la lista de diccionarios con la funcion random
 def simple_list():
-    pass
+    lista_de_diccionarios = []
+    for i in range(10):
+        diccionario = {
+            "id": i + 1,
+            "age": random.randint(1, 100) 
+        }
+        lista_de_diccionarios.append(diccionario)
+    return lista_de_diccionarios
 
-
-def sort_list():
-    pass
+# Ordeno por edad en orden ascendente
+def sort_list(dicts):
+    return sorted(dicts, key=lambda x: x["age"])
