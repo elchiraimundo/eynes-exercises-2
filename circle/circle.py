71195ee2041138e9044a05d46b493a2e3a096443
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-30 11:55:13 -0300

    Borre metodos

--- circle/circle.py
+++ circle/circle.py
@@ -1,30 +1,5 @@
 import math
 
 class Circle:
-
-    def __init__(self, radius):
-        if radius <= 0:
-            raise ValueError("El radio debe ser mayor que 0")
-        self.radius = radius
-
-    def get_radius(self):
-        return self.radius
-    
-    def set_radius(self,radius):
-        if radius <= 0:
-            raise ValueError("El radio debe ser mayor que 0")
-        self.radius = radius
-
-    def get_area(self):
-        return math.pi * self.radius**2 
-    
-    def get_perimeter(self):
-        return math.pi * self.radius * 2
-    
-    def __mul__(self, n):
-        if n <= 0:
-            raise ValueError("El multiplicador debe ser mayor a 0")
-        return Circle(self.radius * n) 
-
-    def __str__(self):
-        return f"El Radio del Circulo es de {self.radius}"
+    pass
+   
